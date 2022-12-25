2ba8a3f6fd53f5d8f9b3f45d31557ada01d085bb
meher09 <mehernigarcu@gmail.com>
2022-12-25 17:34:02 +0600

    removed sample data

--- Wordpress Rest Api.py
+++ Wordpress Rest Api.py
@@ -38,8 +38,3 @@ class WordPressAPI:
         }
 
 
-username = 'admin'
-password = '123 456 789 0123'
-website = 'https://www.mywpsite.com'
-api = WordPressAPI(website, username, password)
-print(api.endpoints['posts'])
